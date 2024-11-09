5e3ba7d67f799c18bf706a1082a30de8868c67f5
CaoVanHotXoan <157797415+CaoVanHotXoan@users.noreply.github.com>
2024-11-09 21:26:54 +0700

    Update main.js

--- java/main.js
+++ java/main.js
@@ -1,17 +1,3 @@
 window.addEventListener("load", () => {
-  Swal.fire({
-    title: "Chỉnh đủ âm lượng để nghe",
-    icon: "warning",
-    showCancelButton: true,
-    confirmButtonColor: "#3085d6",
-    cancelButtonColor: "#d33",
-    confirmButtonText: "OK",
-    cancelButtonText: "Canner",
-  }).then((result) => {
-    if (result.isConfirmed) {
       document.querySelector(".song").play();
-    } else {
-      document.querySelector(".song").play();
-    }
-  });
 });
